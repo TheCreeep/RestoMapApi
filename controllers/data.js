d0cd7fba8eb39be9182470fe0a7eff9f269e1b67
TheCreeep <thecreepfire@gmail.com>
2022-06-24 10:17:49 +0200

    :pencil: | Added Search + Made regex For name insentitive to case

--- controllers/data.js
+++ controllers/data.js
@@ -13,10 +13,30 @@ export const DataController = async function (req, res) {
     }
 }
 
-export const SearchController = async function (req, res) {
+export const SearchByNameController = async function (req, res) {
     const restaurantName = req.query.name;
     try {
-        await RestaurantModel.aggregate([{ $match: { "name": { '$regex': restaurantName } } }, {
+        await RestaurantModel.aggregate([{ $match: { "name": { '$regex': restaurantName, '$options': 'i' } } }, {
+            $project: {
+                _id: 0,
+                cuisine: 1,
+                grades: 1,
+                fullAddress: { $concat: ['$address.building', ' ', '$address.street', ' ', '$address.zipcode'] },
+                fullName: { $concat: ['$name', ' (', { $toUpper: '$borough' }, ')'] }
+            }
+        }]).sort({ fullName: 1 })
+            .then((docs) => {
+                res.json(docs);
+            }
+            );
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export const SearchController = async function (req, res) {
+    try {
+        await RestaurantModel.aggregate([{ $match: req.query }, {
             $project: {
                 _id: 0,
                 cuisine: 1,
--- routes/route.js
+++ routes/route.js
@@ -2,11 +2,12 @@ import express from 'express';
 const router = express.Router();
 
 import HomeController from '../controllers/home.js';
-import { DataController, SearchController } from '../controllers/data.js';
+import { DataController, SearchController, SearchByNameController } from '../controllers/data.js';
 
 router.get('/', HomeController);
 router.get('/data', DataController);
-router.get('/data/name_search', SearchController);
+router.get('/data/search', SearchController);
+router.get('/data/name_search', SearchByNameController);
 
 
 export default router
